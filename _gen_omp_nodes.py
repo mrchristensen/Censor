abfb2810005e07d8355f4ed38c99e849782e28d0
Benjamin Ogles <benjaminogles@gmail.com>
2018-01-23 15:06:25 -0700

    Fix pylint stuff

--- _gen_omp_nodes.py
+++ _gen_omp_nodes.py
@@ -1,6 +1,7 @@
-# Generates nodes for parsing OMP
+"""Generates nodes for parsing OMP"""
 
-from pycparser._ast_gen import ASTCodeGenerator 
+from pycparser._ast_gen import ASTCodeGenerator
 
 omp_gen = ASTCodeGenerator('_omp_ast.cfg')
 omp_gen.generate(open('omp_ast.py', 'w'))
+
