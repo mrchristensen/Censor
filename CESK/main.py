106131d7e9f1ba25c7c3e315523c5d80b29754e1
Kyle Storey <kylona@spicey.byu.edu>
2018-01-26 15:53:58 -0700

    Add CESK directory and a few base files

---
+++ CESK/main.py
@@ -0,0 +1,8 @@
+"""Starts the CESK machine"""
+
+def main():
+    """Is a stub"""
+    print("Not hello World")
+
+if __name__ == "__main__":
+    main()
---
+++ runRaceCompare.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SOURCE_PATH=$1
+TEMP_OUT="tempFromRunRaceCompare.exe"
+LOG="runCompareLog.txt"
+
+if [ -z $SOURCE_PATH ]; then 
+   echo Usage: runRaceCompare path/to/cSource.c
+   exit 0
+fi
+
+gcc -o $TEMP_OUT $SOURCE_PATH -g -fopenmp
+valgrind --tool=helgrind --error-exitcode=1 ./$TEMP_OUT 2>$LOG
+if [ $? -eq 0 ]; then
+   echo Helgrind: NO
+else 
+   echo Helgrind: YES
+fi
+gcc -o $TEMP_OUT $SOURCE_PATH -fsanitize=thread -g -fopenmp
+./$TEMP_OUT 2>> $LOG
+
+if [ $? -eq 0 ]; then
+   echo Thread Sanitizer: NO
+else 
+   echo Thread Sanitizer: YES
+fi
