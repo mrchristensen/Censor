1548817ec0c84176e7d83e3196b15e42d80ce573
Kyle Storey <kylona@spicey.byu.edu>
2018-01-29 11:01:09 -0700

    Add Value abstract Class

--- CESK/structures.py
+++ CESK/structures.py
@@ -1,7 +1,5 @@
 """Holds the data structures for the CESK machine"""
 
-from collections import namedtuple
-
 class State: #pylint:disable=too-few-public-methods
     """Holds a program state"""
     ctrl = None #control
@@ -9,26 +7,48 @@ class State: #pylint:disable=too-few-public-methods
     stor = None #store
     kont = None #k(c)ontinuation
 
-class Cont: #pylint:disable=too-few-public-methods
+    def set_ctrl(self, ctrl):
+        """attaches a control object to the state"""
+        ctrl.attach(self)
+        self.ctrl = ctrl
+
+    def set_envr(self, envr):
+        """attaches an environment object to the state"""
+        envr.attach(self)
+        self.envr = envr
+
+    def set_stor(self, stor):
+        """attaches a stor object to the state"""
+        stor.attach(self)
+        self.stor = stor
+
+class Ctrl: #pylint:disable=too-few-public-methods
     """Holds the control pointer or location of the program"""
+    host_state = None
     statement = "statement;"
     location = "filename:line:char"
     node = None #AST node
 
+    def attach(self, state):
+        """makes the given state its host"""
+        self.host_state = state
+
 class Envr:
     """Holds the enviorment (a maping of identifiers to addresses)"""
-    address_counter = 0
-    IdToAddr = namedtuple("IdToAddr", ['identifier', 'address'])
+    host_state = None
+    map = {} #A set of IdToAddr mappings
 
-    def __init__(self):
-        self.map = {} #A set of IdToAddr mappings
+    def attach(self, state):
+        """makes the given state its host"""
+        self.host_state = state
 
     def map_new_identifier(self, ident):
         """Add a new identifier to the mapping"""
-        # TODO fix this line
-        #self.map.append(IdToAddr(ident, addressCounter))
-        #address_counter += 1
-        #return address_counter - 1
+        if self.host_state is None:
+            raise Exception("Calling method in Envr with no host state")
+        new_address = self.host_state.stor.get_next_address()
+        self.map[ident] = new_address
+        return new_address
 
     def is_defined(self, ident):
         """returns if a given identifier is defined"""
@@ -36,11 +56,23 @@ class Envr:
 
 class Stor:
     """Represents the contents of memory at a moment in time."""
+    host_state = None
+    address_counter = 0
+
     def __init__(self, memory=None):
         if memory is None:
             self.memory = {}
         else:
             self.memory = memory
+
+    def attach(self, state):
+        """makes the given state its host"""
+        self.host_state = state
+
+    def get_next_address(self):
+        """returns the next available storage address"""
+        self.address_counter += 1
+        return self.address_counter - 1
 
     def read(self, address):
         """Read the contents of the store at address. Returns None if undefined.
@@ -59,3 +91,18 @@ class Stor:
         else:
             memory[address] = value
         return Stor(memory)
+
+class Value: #pylint:disable=too-few-public-methods
+    """Abstract class for polymorphism between abstract and concrete values"""
+
+    def __add__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+    def __div__(self, other):
+        pass
