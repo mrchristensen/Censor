787745a6911bd7049e48e438179659c381a34af5
Seth Poulsen <sethpoulsen@bitbucket.com>
2018-02-12 12:40:46 -0700

    CESK: Added more operations to Integer, implemented Float.

--- cesk/values.py
+++ cesk/values.py
@@ -2,21 +2,36 @@
 a value based on an assignment node"""
 import cesk.limits as limits
 
-class Value: #pylint:disable=too-few-public-methods
-    """Abstract class for polymorphism between abstract and concrete values"""
+BINOPS = {
+    "+" : "__add__",
+    "-" : "__sub__",
+    "*" : "__mul__",
+    "/" : "__truediv__",
+    "%" : "__mod__",
+    "<" : "__lt__",
+    "<=": "__le__",
+    "==": "__eq__",
+    "!=": "__ne__",
+    ">" : "__gt__",
+    ">=": "__ge__",
+}
 
+class ArithmeticValue: #pylint:disable=too-few-public-methods
+    """Abstract class for polymorphism between abstract and concrete values"""
+    data = None
+    type_of = None
     def perform_operation(self, operator, value): # pylint: disable=inconsistent-return-statements
         """Performs operation and returns value."""
-        if operator == "+":
-            return self + value
-        elif operator == "-":
-            return self - value
-        elif operator == "*":
-            return self * value
-        elif operator == "/":
-            return self / value
+        if operator in BINOPS:
+            method = self.__getattribute__(BINOPS[operator])
+            return method(value)
         else:
             print("Operator not implemented!")
+
+    def get_truth_value(self):
+        """Returns a bool denoting what truth value the ArithmeticValue would have
+        if it were inside of an if statement in C"""
+        return bool(self.data)
 
     def __add__(self, other):
         pass
@@ -27,14 +42,30 @@ class Value: #pylint:disable=too-few-public-methods
     def __mul__(self, other):
         pass
 
-    def __div__(self, other):
+    def __truediv__(self, other):
         pass
 
+    def __lt__(self, other):
+        return Integer(int(self.data < other.data), 'int')
 
-class Integer(Value): #pylint:disable=too-few-public-methods
+    def __le__(self, other):
+        return Integer(int(self.data <= other.data), 'int')
+
+    def __eq__(self, other):
+        return Integer(int(self.data == other.data), 'int')
+
+    def __ne__(self, other):
+        return Integer(int(self.data != other.data), 'int')
+
+    def __gt__(self, other):
+        return Integer(int(self.data > other.data), 'int')
+
+    def __ge__(self, other):
+        return Integer(int(self.data >= other.data), 'int')
+
+
+class Integer(ArithmeticValue): #pylint:disable=too-few-public-methods
     """Concrete implementation of an Integral Type"""
-    data = None
-    type_of = None
     max_value = limits.INT_MAX
     min_value = limits.INT_MIN
 
@@ -56,27 +87,31 @@ class Integer(Value): #pylint:disable=too-few-public-methods
 
     def __mod__(self, other):
         return Integer(self.data % other.data, self.type_of)
-    def __lt__(self, other):
-        return self.data < other.data
-    def __le__(self, other):
-        return self.data <= other.data
-    def __eq__(self, other):
-        return self.data == other.data
-    def __ne__(self, other):
-        return self.data != other.data
-    def __gt__(self, other):
-        return self.data > other.data
-    def __ge__(self, other):
-        return self.data >= other.data
 
 
-class Float(Value):  #pylint:disable=too-few-public-methods
+
+class Float(ArithmeticValue):  #pylint:disable=too-few-public-methods
     """Concrete implementation of a float.
     NOTE: this class will represent 'float,' and 'double,' and 'long double'
     from C99 as a 64 bit floating point number. This gives some potentially
     incorrect rounding for c programs that use the 'float' or 'long double'
     data types, which is supposed to be represented as a 32 or float."""
-    pass
+
+    def __init__(self, data, type_of):
+        self.data = float(data)
+        self.type_of = type_of
+
+    def __add__(self, other):
+        return Float(self.data + other.data, self.type_of)
+
+    def __sub__(self, other):
+        return Float(self.data - other.data, self.type_of)
+
+    def __mul__(self, other):
+        return Float(self.data * other.data, self.type_of)
+
+    def __truediv__(self, other):
+        return Float(self.data / other.data, self.type_of)
 
 
 class Pointer(Integer):  #pylint:disable=too-few-public-methods
