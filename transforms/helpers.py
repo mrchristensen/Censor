55caeb13a73b048d0feab848437ee19a07b691ba
Seth Poulsen <sethpoulsen@bitbucket.com>
2018-02-21 15:53:05 -0700

    Created helper class to generate unique identifiers for transforms to use.

--- transforms/helpers.py
+++ transforms/helpers.py
@@ -25,3 +25,16 @@ class IncorrectTransformOrder(Exception):
     def __init__(self, message, node=None):
         super().__init__(message)
         self.node = node
+
+class IDGenerator(object): #pylint:disable=too-few-public-methods
+    """Produces identifiers for tranforms to use when they need to declare
+    an extra variable as part of a transform. They are guaranteed to be unique
+    becuase they use a static counter. They are also guaranteed not to collide
+    with any valid identifier in the input program because they begin
+    with a $."""
+    GLOBAL_ID_COUNT = 0
+    @staticmethod
+    def get_unique_id():
+        """Generates a unique id."""
+        IDGenerator.GLOBAL_ID_COUNT += 1
+        return "$" + str(IDGenerator.GLOBAL_ID_COUNT)
