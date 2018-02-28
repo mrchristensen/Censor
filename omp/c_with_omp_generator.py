d57a7b6fe4ce46f1dac75a2b37b065b3023cf175
Benjamin Ogles <benjaminogles@gmail.com>
2018-02-27 18:03:21 -0700

    Implement C code generation for omp pragma nodes

--- omp/c_with_omp_generator.py
+++ omp/c_with_omp_generator.py
@@ -1,92 +1,87 @@
 """Extended CGenerator to support OMP Nodes"""
-
 from pycparser.c_generator import CGenerator
-
 # This class is following a pattern that is required by the pycparser.CGenerator
 # that pylint doesn't like. So:
-#pylint: disable=invalid-name,missing-docstring,too-many-public-methods
-
+#pylint: disable=invalid-name,missing-docstring,too-many-public-methods,no-self-use
 class CWithOMPGenerator(CGenerator):
     """ Uses CGenerator to emit C code from an AST.
         Customized to support OMP Nodes.
-
         From CGenerator: Uses the same visitor pattern as c_ast.NodeVisitor,
         but modified to return a value from each visit method, using string
         accumulation in generic_visit.
     """
-
     def visit_DeclList(self, n):
         """Add semicolon to the end of DeclList"""
         return super().visit_DeclList(n) + ';'
 
     def visit_OmpFor(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpParallel(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpSections(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpSection(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpSingle(self, n):
-        pass
-
-    def visit_OmpSimd(self, n):
-        pass
-
-    def visit_OmpDeclareSimd(self, n):
-        pass
-
-    def visit_OmpForSimd(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpTask(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpTaskloop(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpTaskloopSimd(self, n):
-        pass
-
-    def visit_OmpTaskyield(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpMaster(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpCritical(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpBarrier(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpTaskwait(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpTaskgroup(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpAtomic(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpFlush(self, n):
-        pass
-
-    def visit_OmpOrdered(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpCancel(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpCancellationPoint(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
 
     def visit_OmpThreadprivate(self, n):
-        pass
-
-    def visit_OmpDeclareReduction(self, n):
-        pass
+        """Return pragman string"""
+        return '#pragma ' + n.pragma + '\n' + self.generic_visit(n)
