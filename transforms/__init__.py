48da6c5d61c8da5d15a5ba9ba25c004f5b28c7f3
Daniel Bluhm <bluhmdan@gmail.com>
2018-03-27 16:06:22 -0600

    Separate transform_generator setup into method
    This is helpful for doing each transform one at a time in regression
    testing.

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -45,8 +45,10 @@ from .single_return import SingleReturn
 from .id_generator import IDGenerator
 from .type_environment_calculator import TypeEnvironmentCalculator
 
-def transform(ast):
-    """Perform each transform in package"""
+def get_transformer_generators(ast):
+    """ Return setup tranformer_generators.
+        Separation of this into a method is helpful in regression tests.
+    """
     # one id_generator must be passed to all transforms, to
     # ensure unique ids across transforms
     id_generator = IDGenerator(ast)
@@ -54,7 +56,7 @@ def transform(ast):
     # for a transform, because the AST changes and so the type environments
     # should be different
     type_env_calc = TypeEnvironmentCalculator()
-    transformer_generators = [
+    return [
         lambda: PragmaToOmpParallelSections(),
         lambda: PragmaToOmpParallelFor(),
         lambda: PragmaToOmpParallel(),
@@ -80,6 +82,10 @@ def transform(ast):
         lambda: InsertExplicitTypeCasts(type_env_calc.get_environments(ast)),
         lambda: SingleReturn(id_generator),
     ]
+
+def transform(ast):
+    """Perform each transform in package"""
+    transformer_generators = get_transformer_generators(ast)
     for generator in transformer_generators:
         transformer = generator()
         ast = transformer.visit(ast)
