e2960c28987c8c019021485c7ae302ff5f95248f
Benjamin Ogles <benjaminogles@gmail.com>
2018-04-10 12:14:10 -0600

    Add new line to logging function

--- instrumenter/logger.c
+++ instrumenter/logger.c
@@ -3,5 +3,5 @@ void yeti_log_heap_access(char* mode, void* var, int thread_num, char* name) {
 }
 
 void yeti_log_omp(char* action, char* construct, int thread_num) {
-  printf("%s, %s, %d", action, construct, thread_num);
+  printf("%s, %s, %d\n", action, construct, thread_num);
 }
--- tests/test_instrumenter/fixtures/instrumenter/parallel_simple_golden.c
+++ tests/test_instrumenter/fixtures/instrumenter/parallel_simple_golden.c
@@ -9,7 +9,7 @@ void yeti_log_heap_access(char* mode, void* var, int thread_num, char* name)
 
 void yeti_log_omp(char* action, char* construct, int thread_num)
 {
-  printf("%s, %s, %d", action, construct, thread_num);
+  printf("%s, %s, %d\n", action, construct, thread_num);
 }
 
 int main()
