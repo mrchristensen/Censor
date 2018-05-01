0b1766f2cb23ebce59142c9e0c7138e268c7254f
Benjamin Ogles <benjaminogles@gmail.com>
2018-05-01 14:38:23 -0600

    Fix error in omp_get_ancestor_thread_num call

--- instrumenter/logger.c
+++ instrumenter/logger.c
@@ -1,11 +1,11 @@
 void yeti_log_memory_access(char* mode, void* var) {
   int thread_num = omp_get_thread_num();
-  int ancestor_num = omp_get_ancestor_thread_num();
+  int ancestor_num = omp_get_ancestor_thread_num(omp_get_level()-1);
   printf("%s, %p, %d, %d\n", mode, var, thread_num, ancestor_num);
 }
 
 void yeti_log_omp(char* action, char* construct) {
   int thread_num = omp_get_thread_num();
-  int ancestor_num = omp_get_ancestor_thread_num();
+  int ancestor_num = omp_get_ancestor_thread_num(omp_get_level()-1);
   printf("%s, %s, %d, %d\n", action, construct, thread_num, ancestor_num);
 }
