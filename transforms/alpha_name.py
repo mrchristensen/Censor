73e5fe2ea96a20e1f4899a8b0aba5918ad4a603e
James Wasson <jameswasson@users.noreply.github.com>
2018-07-30 12:52:08 -0600

    Alpha_Name optimization
    Now runs in linear time instead of quadratic time.
    NOTE: The original quadratic time only applied to situations where renaming was needed, so it didn't influence our paper's results.

--- transforms/alpha_name.py
+++ transforms/alpha_name.py
@@ -1,18 +1,18 @@
 '''Renames all variables so that they are unique in
     the function and global scope'''
-import copy
 import pycparser.c_ast as AST  # pylint: disable=wildcard-import, unused-wildcard-import
 from .node_transformer import NodeTransformer
 
 GLOBAL_IDS = set() #IDs specific to global scope
 FUNCTION_IDS = set() #All IDs declared in that function so far
-TO_RENAME_IN_SCOPE = [{}] # record of how to rename variables that need renaming
+TO_RENAME_STACK_MAP = {} # map from an old_id to a stack of new_ids.
+RE_DECLS_IN_SCOPE = [set()] # Stack of sets of all ids of new decls in scopes.
 
 class AlphaName(NodeTransformer):
     """Renames all variables so that functions can contain removable scopes"""
 
     def visit_FileAST(self, node): # pylint: disable=invalid-name
-        '''Find all global variables and add their ids to SET_OF_IDS
+        '''Find all global variables and add their ids to GLOBAL_IDS
             Assume those are unique'''
         function_nodes = []
         for global_node in node.ext:
@@ -30,7 +30,8 @@ class AlphaName(NodeTransformer):
             self.visit(f_node)
 
         GLOBAL_IDS.clear()
-        TO_RENAME_IN_SCOPE[-1].clear()
+        TO_RENAME_STACK_MAP.clear()
+        RE_DECLS_IN_SCOPE[-1].clear()
         return node
 
     def visit_Enumerator(self, node): # pylint: disable=invalid-name
@@ -42,13 +43,19 @@ class AlphaName(NodeTransformer):
         return node
 
     def visit_Compound(self, node): # pylint: disable=invalid-name
-        '''Create new map for things to rename in this scope, put on stack
+        '''Create new set for decls in this scope, put on stack
            do a generic visit
-           delete the map you just made'''
-        map_of_last_scope = TO_RENAME_IN_SCOPE[-1]
-        TO_RENAME_IN_SCOPE.append(copy.deepcopy(map_of_last_scope))
+           pop each stack for each element in set
+           delete the set'''
+        RE_DECLS_IN_SCOPE.append(set())
+
         node = self.generic_visit(node)
-        TO_RENAME_IN_SCOPE.pop()
+
+        ids_to_remove = RE_DECLS_IN_SCOPE.pop()
+        for name in ids_to_remove:
+            TO_RENAME_STACK_MAP[name].pop()
+            if not TO_RENAME_STACK_MAP[name]:
+                del TO_RENAME_STACK_MAP[name]
         return node
 
     def visit_Decl(self, node): # pylint: disable=invalid-name
@@ -69,8 +76,8 @@ class AlphaName(NodeTransformer):
 
     def visit_ID(self, node): # pylint: disable=invalid-name
         '''Check ID to see if it should be changed, and change it.'''
-        if node.name in TO_RENAME_IN_SCOPE[-1]:
-            node.name = TO_RENAME_IN_SCOPE[-1][node.name]
+        if node.name in TO_RENAME_STACK_MAP:
+            node.name = TO_RENAME_STACK_MAP[node.name][-1]
         return self.generic_visit(node)
 
     def visit_FuncDef(self, node): # pylint: disable=invalid-name
@@ -84,13 +91,18 @@ class AlphaName(NodeTransformer):
         '''finds a name not declared yet, places in map
             add to funtion IDs'''
         old_name = decl.name
+        if not old_name in TO_RENAME_STACK_MAP:
+            TO_RENAME_STACK_MAP[old_name] = []
+
         try_count = 0
         new_name = old_name + '_' + str(try_count)
         while new_name in FUNCTION_IDS:
             try_count += 1
             new_name = old_name + '_' + str(try_count)
+
         FUNCTION_IDS.add(new_name)
-        TO_RENAME_IN_SCOPE[-1][old_name] = new_name
+        RE_DECLS_IN_SCOPE[-1].add(old_name)
+        TO_RENAME_STACK_MAP[old_name].append(new_name)
         decl.name = new_name
         if isinstance(decl, AST.Decl):
             decl.type.declname = new_name
