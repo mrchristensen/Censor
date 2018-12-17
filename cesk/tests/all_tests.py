ce8b899cb5e3719c76aa7c687959578cf15f579d
Benjamin Ogles <benjaminogles@gmail.com>
2018-12-17 13:01:37 -0700

    Add OpenMP tests to all_tests

--- cesk/tests/all_tests.py
+++ cesk/tests/all_tests.py
@@ -10,7 +10,8 @@ SUITE = unittest.TestLoader().loadTestsFromNames(
         'basic_functionality',
         'continuation_edge_cases',
         'transform_checks',
-        'difficult_pointer'
+        'difficult_pointer',
+        'omp'
     ]
 )
 TESTRESULT = unittest.TextTestRunner(verbosity=1).run(SUITE)
