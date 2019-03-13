9d0a42d82610607243fd187df1a030f26f8ee3f5
Matt Christensen <mr.christensen@gmail.com>
2019-03-13 15:59:57 -0600

    Fixed new redefs

--- cesk/tests/fixtures/ssa_functionality/01_negative_define.c
+++ cesk/tests/fixtures/ssa_functionality/01_negative_define.c
@@ -2,14 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef int uint32_t;
+
 void fun(int myInt) {
 	return;
 }
 
+extern int nla_total_size(int payload);
+extern int nla_put_u32(struct nl_msg *, int, uint32_t);
+
 int main() {
 	#define TEST 1
 	int a = -TEST;
-	printf("%d", a);
-	fun(-TEST);
+	//uint32_t b = a;
+	//int c = nla_total_size(sizeof(a));
+	//printf("%d", a);
+	//printf("%d", b);
+	//printf("%d", c);
+	int d = 0;
+	switch(a){
+	case -TEST:
+		d = -TEST;
+	break;
+	}
+	fun(d);
+	printf("%d", d);
 	return 0;
 }
