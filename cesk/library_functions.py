8434db7dee36323b1a2861e44898ddf4607a33f9
Matt Christensen <mr.christensen@gmail.com>
2019-04-11 11:48:27 -0600

    Start of hashmap_get() mock

--- cesk/library_functions.py
+++ cesk/library_functions.py
@@ -1,9 +1,20 @@
 """ Set of cstd library functions with a CESK specific implementation:
 need the state and an array containing the values of the arguements passed """
+import logging
 import re
 import random
 import cesk.values as values
 
+
+def hashmap_get(state, args, return_address):#pylint: disable=unused-argument
+    '''mocks the functionality of hashmap_get'''
+    logging.debug("Args of hashmap_get " + str(args))
+    logging.debug("state of hashmap_get " + str(state))
+    logging.debug("return_address of hashmap_get " + str(return_address))
+    value = values.generate_null_pointer()
+    state.stor.write(return_address, value)
+    state.stor.write(return_address, value)
+    return {state.get_next()}, {}
 
 def printf(state, args, return_address):#pylint: disable=unused-argument
     '''performs printf'''
