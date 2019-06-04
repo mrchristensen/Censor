ccbed30709b3f7e26dd5f274eff54d0b1ffa29c4
Peter Aldous <aldous@cs.byu.edu>
2019-06-04 14:15:47 -0600

    added a demo script and test files

---
+++ demo/example.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct s
+{
+    int a;
+    int b;
+    int c;
+};
+
+
+void f(int *x)
+{
+    printf("%d\n", *(x + 1));
+}
+int main()
+{
+    struct s x = {1, 2, 3};
+    int* y = &x;
+    f(y + 1); // safe
+    f(y + 2); // unsafe
+    return 0;
+}
+
